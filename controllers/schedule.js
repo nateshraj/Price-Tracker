c37e2ecebf18bb3fbf293e91e43820149c1d1046
Natesh Raj <nateshraj.1996@gmail.com>
2020-02-07 19:44:32 +0530

    Schedule test

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -3,15 +3,16 @@ const axios = require('axios');
 
 const loginAndRefreshPrices = async () => {
   try {
+    const port = process.env.PORT || 5000;
     const config = {
       headers: { 'Content-Type': 'application/json' }
     }
     const formData = { email: process.env.ADMIN_EMAIL, password: process.env.ADMIN_PASSWORD };
-    const res = await axios.post('http://localhost:5000/api/login', formData, config);
+    const res = await axios.post(`http://localhost:${port}/api/login`, formData, config);
     axios.defaults.headers.common['Authorization'] = res.data.token;
-    await axios.get('http://localhost:5000/api/products/refresh', config);
+    await axios.get(`http://localhost:${port}/api/products/refresh`, config);
   } catch (e) {
-    console.log(e);
+    console.log(`Unable to refresh prices - ${e.message}`);
   }
 }
 
